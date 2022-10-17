131eebc84c42b6d514e12bf4a506909ce65e4db4
Leonardo Narciso <LNarcisoOV@gmail.com>
2022-10-17 11:20:26 +0000

    Add more examples

--- src/com/java11/httpclient/HttpClientMain.java
+++ src/com/java11/httpclient/HttpClientMain.java
@@ -11,63 +11,73 @@ import java.time.Duration;
 public class HttpClientMain {
 
 	private static final HttpClient httpClient = HttpClient.newBuilder().build();
-	
+
 	public static void main(String... args) throws IOException, InterruptedException {
-		
-		System.out.println(" --------------------------------- ");
+		addRepeatedTexts();
 		System.out.println(" ------------HTTPCLIENT----------- ");
-		
-		HttpRequest request = HttpRequest.newBuilder()
+
+		HttpRequest request = HttpRequest
+				.newBuilder()
 				.GET()
 				.uri(URI.create("https://www.google.com/"))
 				.build();
-		
+
 		HttpResponse<String> response = httpClient.send(request, BodyHandlers.ofString());
-		
-		System.out.println("Response code: " + response.statusCode() );
+
+		System.out.println("Response code: " + response.statusCode());
 		System.out.println("Response body: " + response.body());
-		
-		
-		System.out.println();
-		System.out.println();
-		System.out.println(" --------------------------------- ");
+
+		addRepeatedTexts();
 		System.out.println(" --------------XML---------------- ");
-		
-		
+
 		HttpRequest request2 = HttpRequest.newBuilder()
 				.GET()
 				.uri(URI.create("https://apichallenges.herokuapp.com/todos"))
 				.header("accept", "application/xml")
 				.build();
-		
+
 		HttpResponse<String> response2 = httpClient.send(request2, BodyHandlers.ofString());
-		
-		System.out.println("Response code: " + response2.statusCode() );
+
+		System.out.println("Response code: " + response2.statusCode());
 		System.out.println("Response body: " + response2.body());
-	
-		System.out.println(" --------------------------------- ");
-		System.out.println(" --------------------------------- ");
+
+		
+
+		addRepeatedTexts();
+		System.out.println(" --------DURATION--TIMEOUT-------- ");
+
+		HttpClient httpClient2 = HttpClient.newBuilder()
+				.connectTimeout(Duration.ofSeconds(3))
+				.build();
+
+		HttpRequest request4 = HttpRequest.newBuilder()
+				.GET()
+				.uri(URI.create("https://www.getpostman.com/"))
+				.header("accept", "application/xml")
+				.timeout(Duration.ofSeconds(3)).build();
+
+		HttpResponse<String> response4 = httpClient2.send(request4, BodyHandlers.ofString());
+
+		System.out.println("Response code: " + response4.statusCode());
+		System.out.println("Response body: " + response4.body());
 		
 		
-		System.out.println();
-		System.out.println();
-		System.out.println(" --------------------------------- ");
-		System.out.println(" -------------TIMEOUT------------- ");
-		
-		
+		addRepeatedTexts();
+		System.out.println(" --------RESPONSE--TIMEOUT-------- ");
+
 		HttpRequest request3 = HttpRequest.newBuilder()
 				.GET()
 				.uri(URI.create("https://cat-fact.herokuapp.com/facts/"))
 				.header("accept", "application/xml")
 				.timeout(Duration.ofSeconds(3))
 				.build();
-		
-		HttpResponse<String> response3 = httpClient.send(request3, BodyHandlers.ofString());
-		
-		System.out.println("Response code: " + response3.statusCode() );
-		System.out.println("Response body: " + response3.body());
+
+		httpClient.send(request3, BodyHandlers.ofString());
+	}
 	
-		System.out.println(" --------------------------------- ");
+	private static void addRepeatedTexts() {
+		System.out.println();
+		System.out.println();
 		System.out.println(" --------------------------------- ");
 	}
 
